e40e083d2f30aa5ee739e1383e653010a63eef26
Cristián Yuseff <cr.yuseff@gmail.com>
2018-09-24 16:38:19 -0300

    refactor to promises

--- src/app/zf-terms/zf-terms.component.ts
+++ src/app/zf-terms/zf-terms.component.ts
@@ -11,113 +11,106 @@ import { TermsActions } from './zf-terms-actions';
   templateUrl: './zf-terms.component.html',
   styles: ['ul.disabled { color: #ccc; }']
 })
-export class ZfTermsComponent implements ZfComponentInterface, OnInit, OnDestroy {
+export class ZfTermsComponent implements ZfComponentInterface, OnInit {
   @Input() data: string[];
   @Input() disabled: boolean;
-  @Input() error: Subject<ZfError>;
   @Output() changes = new Subject<ZfData>();
 
   @ViewChild('termInput') termInputRef: ElementRef;
 
   errorMessage: string;
   errorIndex: number;
-  private errorSub: Subscription;
 
   ngOnInit() {
-    if (!this.error) {
-      return;
-    }
+    // this.errorSub = this.error.subscribe(
+    //   (err: ZfError) => {
+    //     console.log('Err received', err);
+    //     // handle error
+    //     if (err instanceof ErrAdd) {
+    //       console.log('Handle ErrAdd');
+    //       const input = this.termInputRef.nativeElement as HTMLInputElement;
+    //       input.value = err.meta.newValue;
+    //       this.errorMessage = err.message;
+    //       return;
+    //     }
 
-    this.errorSub = this.error.subscribe(
-      (err: ZfError) => {
-        console.log('Err received', err);
-        // handle error
-        if (err instanceof ErrAdd) {
-          console.log('Handle ErrAdd');
-          const input = this.termInputRef.nativeElement as HTMLInputElement;
-          input.value = err.meta.newValue;
-          this.errorMessage = err.message;
-          return;
-        }
+    //     if (err instanceof ErrUpdate) {
+    //       console.log('Handle ErrUpdate');
+    //       this.errorIndex = err.meta.idx;
+    //       this.errorMessage = err.message;
+    //       return;
+    //     }
 
-        if (err instanceof ErrUpdate) {
-          console.log('Handle ErrUpdate');
-          this.errorIndex = err.meta.idx;
-          this.errorMessage = err.message;
-          return;
-        }
-
-        if (err instanceof ErrDelete) {
-          console.log('Handle ErrDelete');
-          this.errorIndex = err.meta.idx;
-          this.errorMessage = err.message;
-          return;
-        }
-      }
-    );
-  }
-
-  ngOnDestroy() {
-    this.errorSub.unsubscribe();
+    //     if (err instanceof ErrDelete) {
+    //       console.log('Handle ErrDelete');
+    //       this.errorIndex = err.meta.idx;
+    //       this.errorMessage = err.message;
+    //       return;
+    //     }
+    //   }
+    // );
   }
 
   add(termInput: HTMLInputElement) {
     const { value } = termInput;
     const result = [...this.data, value];
 
-    const err = new ErrAdd({newValue: value});
+    const data = new ZfData(ZfActions.Add, null, value, result);
 
-    const data: ZfData = {
-      action: ZfActions.Add,
-      oldValue: null,
-      newValue: value,
-      result,
-      error: err
-    };
+    const promise = new Promise((resolve, reject) => {
+      data.promise = {resolve, reject};
+    });
 
-    termInput.value = '';
-    this.errorMessage = '';
+    promise
+      .then(() => {
+        termInput.value = '';
+        this.errorMessage = '';
+      })
+      .catch((err: Error) => {
+        console.log(err);
+        this.errorMessage = err.message;
+      });
 
     this.changes.next(data);
   }
 
   update(idx: number, updateInput: HTMLInputElement) {
-    const value = updateInput.value;
-    const result = [...this.data];
-    const oldValue = result[idx];
-    result[idx] = value;
+    // const value = updateInput.value;
+    // const result = [...this.data];
+    // const oldValue = result[idx];
+    // result[idx] = value;
 
-    const err = new ErrUpdate({idx, oldValue, newValue: value});
+    // const err = new ErrUpdate({idx, oldValue, newValue: value});
 
-    const data: ZfData = {
-      action: ZfActions.Update,
-      oldValue,
-      newValue: value,
-      result,
-      error: err
-    };
+    // const data: ZfData = {
+    //   action: ZfActions.Update,
+    //   oldValue,
+    //   newValue: value,
+    //   result,
+    //   error: err
+    // };
 
-    this.errorIndex = null;
+    // this.errorIndex = null;
 
-    this.changes.next(data);
+    // this.changes.next(data);
   }
 
   remove(idx: number) {
-    const result = [...this.data];
-    const oldValue = result.splice(idx, 1)[0];
+    // const result = [...this.data];
+    // const oldValue = result.splice(idx, 1)[0];
 
-    const err = new ErrDelete({idx});
+    // const err = new ErrDelete({idx});
 
-    const data: ZfData = {
-      action: ZfActions.Delete,
-      oldValue,
-      newValue: null,
-      result,
-      error: err
-    };
+    // const data: ZfData = {
+    //   action: ZfActions.Delete,
+    //   oldValue,
+    //   newValue: null,
+    //   result,
+    //   error: err
+    // };
 
-    this.errorIndex = null;
+    // this.errorIndex = null;
 
-    this.changes.next(data);
+    // this.changes.next(data);
   }
 }
